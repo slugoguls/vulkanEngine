81f220be796bf99ba78d5ca733da554cfe878e41
varun chaaras <chaarasvarun@gmail.com>
2025-09-15 18:31:01 +0530

    updated the render loop for bg
    added push conts in the render loop

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -194,6 +194,11 @@ void VulkanEngine::draw_background(VkCommandBuffer cmd)
     // bind the descriptor set containing the draw image for the compute pipeline
     vkCmdBindDescriptorSets(cmd, VK_PIPELINE_BIND_POINT_COMPUTE, _gradientPipelineLayout, 0, 1, &_drawImageDescriptors, 0, nullptr);
 
+    ComputePushConstants pc;
+    pc.data1 = glm::vec4(1, 0, 0, 1);
+    pc.data2 = glm::vec4(0, 0, 1, 1);
+
+    vkCmdPushConstants(cmd, _gradientPipelineLayout, VK_SHADER_STAGE_COMPUTE_BIT, 0, sizeof(ComputePushConstants), &pc);
     // execute the compute pipeline dispatch. We are using 16x16 workgroup size so we need to divide by it
     vkCmdDispatch(cmd, std::ceil(_drawExtent.width / 16.0), std::ceil(_drawExtent.height / 16.0), 1);
 }
@@ -561,7 +566,7 @@ void VulkanEngine::init_background_pipelines()
     VK_CHECK(vkCreatePipelineLayout(_device, &computeLayout, nullptr, &_gradientPipelineLayout));
 
     VkShaderModule computeDrawShader;
-    if (!vkutil::load_shader_module("../shaders/gradient.comp.spv", _device, &computeDrawShader))
+    if (!vkutil::load_shader_module("../shaders/gradient_color.comp.spv", _device, &computeDrawShader))
     {
         fmt::print("Error when building the compute shader \n");
     }
