56cab5a9539e97a7e39f949bafaba9834126354f
varun chaaras <chaarasvarun@gmail.com>
2025-09-17 20:11:22 +0530

    gltf loader class

--- src/vk_loader.h
+++ src/vk_loader.h
@@ -1,1 +1,19 @@
 ﻿#pragma once
+#include <vk_types.h>
+#include <unordered_map>
+#include <filesystem>
+
+struct GeoSurface {
+    uint32_t startIndex;
+    uint32_t count;
+};
+
+struct MeshAsset {
+    std::string name;
+
+    std::vector<GeoSurface> surfaces;
+    GPUMeshBuffers meshBuffers;
+};
+
+//forward declaration
+class VulkanEngine;
