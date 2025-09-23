0c055d1a7d7c4f94a4b9218a67e3a44e050132d7
varun chaaras <chaarasvarun@gmail.com>
2025-09-23 14:12:08 +0530

    new GLTF load func

--- src/vk_loader.h
+++ src/vk_loader.h
@@ -1,7 +1,41 @@
-﻿#pragma once
+#pragma once
 #include <vk_types.h>
+#include <vk_descriptors.h>
 #include <unordered_map>
 #include <filesystem>
+
+// forward declarations
+struct DrawContext;
+class VulkanEngine;
+
+class IRenderable {
+public:
+    virtual ~IRenderable() = default;
+    virtual void Draw(const glm::mat4& topMatrix, DrawContext& ctx) = 0;
+};
+
+class Node : public IRenderable {
+public:
+    // parent pointer must be a weak pointer to avoid circular dependencies
+    std::weak_ptr<Node> parent;
+    std::vector<std::shared_ptr<Node>> children;
+
+    glm::mat4 localTransform;
+    glm::mat4 worldTransform;
+
+    void refreshTransform(const glm::mat4& parentMatrix) {
+        worldTransform = parentMatrix * localTransform;
+        for (auto c : children) {
+            c->refreshTransform(worldTransform);
+        }
+    }
+
+    virtual void Draw(const glm::mat4& topMatrix, DrawContext& ctx) {
+        for (auto& c : children) {
+            c->Draw(topMatrix, ctx);
+        }
+    }
+};
 
 struct GLTFMaterial {
     MaterialInstance data;
@@ -21,9 +55,33 @@ struct MeshAsset {
     GPUMeshBuffers meshBuffers;
 };
 
-//forward declaration
-class VulkanEngine;
+struct LoadedGLTF : public IRenderable {
+
+    // storage for all the data on a given glTF file
+    std::unordered_map<std::string, std::shared_ptr<MeshAsset>> meshes;
+    std::unordered_map<std::string, std::shared_ptr<Node>> nodes;
+    std::unordered_map<std::string, AllocatedImage> images;
+    std::unordered_map<std::string, std::shared_ptr<GLTFMaterial>> materials;
+
+    // nodes that dont have a parent, for iterating through the file in tree order
+    std::vector<std::shared_ptr<Node>> topNodes;
+
+    std::vector<VkSampler> samplers;
+
+    DescriptorAllocatorGrowable descriptorPool;
+
+    AllocatedBuffer materialDataBuffer;
+
+    VulkanEngine* creator;
+
+    ~LoadedGLTF() { clearAll(); };
+
+    virtual void Draw(const glm::mat4& topMatrix, DrawContext& ctx);
+
+private:
+
+    void clearAll();
+}; 
 
 //funcs
 std::optional<std::vector<std::shared_ptr<MeshAsset>>> loadGltfMeshes(VulkanEngine* engine, std::filesystem::path filePath);
-
