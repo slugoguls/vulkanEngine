10f487b74f3e4f8ada2d6d178ed04137113685b4
varun chaaras <chaarasvarun@gmail.com>
2025-10-01 20:34:59 +0530

    cube class

--- engine/primitives/Cube/Cube.h
+++ engine/primitives/Cube/Cube.h
@@ -1,17 +1,27 @@
-// Cube.h
 #pragma once
 #include <vector>
+#include <vulkan/vulkan.h>
 #include <glm/glm.hpp>
-#include "Vertex.h"  
+#include "../Vertex.h"
 
 class Cube {
 public:
-    Cube(float size = 1.0f);
+    Cube(VkDevice device, VkPhysicalDevice physicalDevice, float size = 1.0f);
+    ~Cube();
 
-    const std::vector<Vertex>& getVertices() const { return vertices; }
-    const std::vector<uint32_t>& getIndices() const { return indices; }
+    void draw(VkCommandBuffer cmd, VkPipelineLayout pipelineLayout, const glm::mat4& modelMatrix);
 
 private:
-    std::vector<Vertex> vertices;
-    std::vector<uint32_t> indices;
+    VkDevice device;
+    VkBuffer vertexBuffer;
+    VkDeviceMemory vertexMemory;
+    VkBuffer indexBuffer;
+    VkDeviceMemory indexMemory;
+    uint32_t indexCount;
+
+    void createBuffers(VkPhysicalDevice physicalDevice,
+        const std::vector<Vertex>& vertices,
+        const std::vector<uint32_t>& indices);
+
+    void destroyBuffers();
 };
--- engine/primitives/Vertex.h
+++ engine/primitives/Vertex.h
@@ -3,19 +3,7 @@ struct Vertex {
     glm::vec3 normal;
     glm::vec2 uv;
 
-    static VkVertexInputBindingDescription getBindingDescription() {
-        VkVertexInputBindingDescription bindingDesc{};
-        bindingDesc.binding = 0;
-        bindingDesc.stride = sizeof(Vertex);
-        bindingDesc.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
-        return bindingDesc;
-    }
-
-    static std::array<VkVertexInputAttributeDescription, 3> getAttributeDescriptions() {
-        std::array<VkVertexInputAttributeDescription, 3> attr{};
-        attr[0].binding = 0; attr[0].location = 0; attr[0].format = VK_FORMAT_R32G32B32_SFLOAT; attr[0].offset = offsetof(Vertex, position);
-        attr[1].binding = 0; attr[1].location = 1; attr[1].format = VK_FORMAT_R32G32B32_SFLOAT; attr[1].offset = offsetof(Vertex, normal);
-        attr[2].binding = 0; attr[2].location = 2; attr[2].format = VK_FORMAT_R32G32_SFLOAT;    attr[2].offset = offsetof(Vertex, uv);
-        return attr;
+    Vertex(glm::vec3 pos, glm::vec3 norm, glm::vec2 tex)
+        : position(pos), normal(norm), uv(tex) {
     }
 };
